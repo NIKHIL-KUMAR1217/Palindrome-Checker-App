567ea50b5bbc27eb43bd155fbfc157d26db0c883
NIKHIL-KUMAR1217 <ajay.2005nikhil@gmail.com>
2026-02-24 15:26:40 +0530

    [NIKHIL078]UC2-HardCodePalidrome

--- app/src/palindromeCheckerApp.java
+++ app/src/palindromeCheckerApp.java
@@ -1,5 +1,27 @@
+import java.util.*;
 public class palindromeCheckerApp {
     public static void main (String args[]){
         System.out.println("WELCOME TO PALINDROME CHECKER APP MANAGEMENT SYSTEM");
+        String input = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter a string");
+        input=in.nextLine();
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = input.length() - 1;
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
     }
 }
